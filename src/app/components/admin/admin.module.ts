68017954d86e24dfb948a7b646adaaa08b43fac3
juan <juanhatumvarela@gmail.com>
2022-02-24 12:54:52 -0500

    se crearon los componentes de astronautas y pasajeros

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import {SharedModule} from '../shared/shared.module';
+import { AstronautsComponent } from './astronauts/astronauts.component';
+import { PassengersComponent } from './passengers/passengers.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AstronautsComponent, PassengersComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    SharedModule
   ]
 })
 export class AdminModule { }
